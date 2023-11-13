bcb9da4812843be72019bf469408fe57478f0ebb
Salome Kochalidze <salome.kochalidze9003@gmail.com>
2023-11-13 22:24:59 +0400

    add second file

---
+++ davaleba-2-3-4.js
@@ -0,0 +1,27 @@
+// davaleba-2
+let pupil=[
+    {
+        "name":"Salomeko",
+        "age":19,
+        "address":"Chavchavadze St"
+    },
+    { 
+        "name":"Rubiko",
+        "age":22,
+        "address":"Chavchavadze St"
+    },
+    {
+        "name":"Nanako",
+        "age":16,
+        "address":"Chavchavadze St"
+    }
+];
+// dvaleba-3
+console.log(`My name is ${pupil[0].name}.\ My age is ${pupil[0].age}.\ My address is  ${pupil[0].address}.`);
+// dvaleba-4
+if(pupil[1].age<19){
+    console.log("I am a teenager.");
+}
+else{
+    console.log("I am an adult.")
+}
